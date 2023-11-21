f5a878f7693c7245e662a4111b3921db8a8e2c31
응애원 <107863505+mynameisyewon@users.noreply.github.com>
2023-11-21 14:02:37 +0900

    Update successIndex.js 876

--- client/successIndex.js
+++ client/successIndex.js
@@ -38,7 +38,7 @@ function refreshPage() {
 
       
 
-//const accessToken = localStorage.getItem('accessToken');
+const accessToken = localStorage.getItem('accessToken');
 // const serverUrl = "https://racconworld.com:18646/userInfo/Id_NickName_Balance";
 // //URL이 "http://" 또는 "https://"로 시작하지 않으면 "http://"를 추가
 // if (!serverUrl.startsWith("http://")) {
